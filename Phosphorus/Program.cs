56e343c123cef3073c88345800b77c1004263dbd
John Tur <reflectron@outlook.com>
2017-08-18 16:08:29 -0400

    Removed some unused using statements

--- Phosphorus/Program.cs
+++ Phosphorus/Program.cs
@@ -2,7 +2,6 @@
 using Discord.WebSocket;
 using System;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Phosphorus
--- Phosphorus/Tools/GetTools.cs
+++ Phosphorus/Tools/GetTools.cs
@@ -1,7 +1,6 @@
 ﻿using Discord;
 using Discord.WebSocket;
 using NinjaNye.SearchExtensions;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
